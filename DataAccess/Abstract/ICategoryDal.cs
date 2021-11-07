65c5843f22d8a336600b44f73be2e52c16a9d13c
batuhansarica <73408243+batuhansarica@users.noreply.github.com>
2021-11-07 18:16:23 +0300

    7.11.2021

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public interface ICategoryDal : IEntityRepository<Category>
+    {
+        
+
+    }
+}
